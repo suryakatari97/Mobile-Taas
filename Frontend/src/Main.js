9b0b1478fcf21868d25a16500a32e6ec7de203ea
akhilasanka <akhila.sanka@sjsu.edu>
2019-11-08 22:37:10 -0800

    added bootstrap,axios and react-cookie dependencies and created tester home page

--- Frontend/src/Main.js
+++ Frontend/src/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 import Sidebar from './Views/navigation/Sidebar';
+import TesterHome from './Views/home/TesterHome';
 
 class Main extends Component {
     render(){
         return(
             <div>
                 <Route path = "/sidebar" component = {Sidebar} />
+                <Route path="/tester/home" component={TesterHome} />
             </div>
         )
     }
