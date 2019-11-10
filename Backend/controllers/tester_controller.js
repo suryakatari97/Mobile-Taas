1d4e367bbe3cf21828e2f03bb9abf2b4d0354ba4
akhilasanka <akhila.sanka@sjsu.edu>
2019-11-10 00:31:52 -0800

    added backend functionality for tester home page

--- Backend/controllers/tester_controller.js
+++ Backend/controllers/tester_controller.js
@@ -1,6 +1,24 @@
 const testlogin = (req, res, next)=>{
     res.send({userid: req.user.userid, role: req.user.role});
 };
+
+const getTesterProjects = (req,res,next) =>{
+    console.log(req.query);
+    var testerid = req.query.id;
+    const mysqlconnection = req.db;
+    mysqlconnection.query('SELECT m.projectid, p.projectname FROM cmpe_project_members as m LEFT JOIN cmpe_project as p ON m.projectid = p.projectid WHERE userid=?',
+        [testerid], (err, rowsOfTable, fieldsOfTable)=>{
+            if(err){
+                console.log(err);
+                res.status(500).json({ responseMessage: 'Database not responding' });
+            } else {
+                  console.log(rowsOfTable);
+                  res.status(200).json({ projects: rowsOfTable });
+                }
+    });
+};
+
 module.exports = {
-    testlogin
+    testlogin,
+    getTesterProjects
 };
--- Backend/routes/tester_routes.js
+++ Backend/routes/tester_routes.js
@@ -3,9 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 // user defined modules
-var {testlogin} = require("../controllers/tester_controller");
+var {testlogin, getTesterProjects} = require("../controllers/tester_controller");
 
 // test login
-router.post('/testlogin', testlogin)
+router.post('/testlogin', testlogin);
+
+// get tester projects
+router.get('/home', getTesterProjects);
 
 module.exports = router;
