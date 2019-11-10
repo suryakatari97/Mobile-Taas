6f7bbcbbf96a6781b725d5cec68bbd637f140310
Vibha Ravindra <vibhashree.ravindra@sjsu.edu>
2019-11-09 23:15:40 -0800

    Admin Dashboard with all charts and relevant tables

--- Frontend/src/Views/dashboard/AdminDashboard.js
+++ Frontend/src/Views/dashboard/AdminDashboard.js
@@ -1,12 +1,76 @@
 import React, { Component } from 'react';
+import UsersPerDayGraph from "./UsersPerDayGraph";
+import ProjectsPerDayGraph from "./ProjectsPerDayGraph";
+import TestsPerDayGraph from "./TestsPerDayGraph";
+import BugsPerDayGraph from "./BugsPerDayGraph";
+import ProjectsTopTestCases from "./ProjectsTopTestCases";
+import ProjectsMostTesters from "./ProjectsMostTesters";
+import UserPieChart from "./UserPieChart";
 
 class AdminDashboard extends Component {
     constructor(props){
         super(props);
     }
-    render(){
-        //todo yet to be implemented
-        return(<div><p> Admin yet to be implemented.</p></div>) 
+    render() {
+
+        return (
+            <div>
+            <h1 style={{fontWeight: "700", margin: "0.5em"}}>Administrator Dashboard</h1>
+            <div className='rowC' style={{ display: "flex", flexDirection: "row" }}>
+                
+                <div className="container">
+                    <div className="row mt-4">
+                        <div className="card card-custom mx-4 mb-5" style={{ boxShadow: "2px 2px 2px #888888", "height": "18em", "width": "30em" }}>
+
+                            <div className="card-body" >
+                                <UserPieChart />
+                            </div>
+                        </div>
+
+                        <div className="card card-custom mx-4 mb-5" style={{ boxShadow: "2px 2px 2px #888888", "height": "18em", "width": "30em" }}>
+
+                            <div className="card-body" >
+                                <UsersPerDayGraph />
+                            </div>
+                        </div>
+
+                        <div className="card card-custom mx-4 mb-5" style={{ boxShadow: "2px 2px 2px #888888", "height": "18em", "width": "30em" }}>
+
+                            <div className="card-body" >
+                                <ProjectsPerDayGraph />
+                            </div>
+                        </div>
+
+                        <div className="card card-custom mx-4 mb-5" style={{ boxShadow: "2px 2px 2px #888888", "height": "18em", "width": "30em" }}>
+
+                            <div className="card-body" >
+                                <TestsPerDayGraph />
+                            </div>
+                        </div>
+
+                        <div className="card card-custom mx-4 mb-5" style={{ boxShadow: "2px 2px 2px #888888", "height": "18em", "width": "30em" }}>
+
+                            <div className="card-body" >
+                                <BugsPerDayGraph />
+                            </div>
+                        </div>
+                        <div className="card card-custom mx-4 mb-5" style={{ boxShadow: "2px 2px 2px #888888", "height": "18em", "width": "30em" }}>
+
+                            <div className="card-body" >
+                                <ProjectsTopTestCases/>
+                            </div>
+                        </div>
+                        <div className="card card-custom mx-4 mb-5" style={{ boxShadow: "2px 2px 2px #888888", "height": "18em", "width": "30em" }}>
+
+                            <div className="card-body" >
+                                <ProjectsMostTesters/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        )
     }
 }
 
---
+++ Frontend/src/Views/dashboard/ProjectsMostTesters.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+const axios = require('axios');
+class ProjectsMostTesters extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data:[]
+        }
+    }
+    componentDidMount() {
+        axios.get("/admin/stats/topprojects/testers?limit=5", {headers: 
+          {"Authorization": "Bearer "+localStorage.getItem("jwtToken")}})
+        .then((response) => {
+            if(response.data.success) {
+                this.setState({data: response.data.data});
+            }
+        })
+    }
+
+    render() {
+        let tableContents = this.state.data.map((project) => {
+            return(
+                <tr>
+                    <td>project.projectname</td>
+                    <td>project.status</td>
+                    <td>project.count</td>
+                </tr>
+            )
+        })
+        return(
+            <table class="table">
+             <caption>Projects with most number of testers</caption>
+                 <thead>
+                    <tr>
+                        <th scope="col">Project Name</th>
+                        <th scope="col">Project Status</th>
+                        <th scope="col">No. of testers</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContents}                    
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default ProjectsMostTesters;
