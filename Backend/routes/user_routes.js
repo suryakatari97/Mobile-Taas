088875f37f8497f5eddfc4867f5bc3f024c99e7e
Vibha Ravindra <vibhashree.ravindra@sjsu.edu>
2019-11-09 20:21:48 -0800

    updated user routes

--- Backend/routes/user_routes.js
+++ Backend/routes/user_routes.js
@@ -1,14 +1,16 @@
 "use strict";
 var express = require("express");
 var router = express.Router();
+const multer = require('multer');
+const FormData = multer({ dest: './uploads/' });
 
 // user defined modules
 var {login, signup} = require("../controllers/user_controller");
 
 // login controller
-router.post('/login', login)
+router.post('/login', FormData.none(), login)
 
 // signup
-router.post("/signup", signup)
+router.post("/signup", FormData.none(), signup)
 
 module.exports = router;
